e5a5637e9095341db06a8861888bc5e82997a189
Neeraja <neerusuguna@gmail.com>
2025-09-03 20:48:04 +0530

    initial set up

---
+++ src/test/java/com/neeru/rate_limiting_api/RateLimitingApiApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.neeru.rate_limiting_api;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class RateLimitingApiApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
